ae4da5ec98857f0652516b3c8b09401dc3e21e8d
victorkimuyu <victorkimuyu@gmail.com>
2021-01-30 18:01:04 +0300

    Part 4: Basic CRUD: Generic Class based views II
    Class based views abstract the most common CRUD operations to simplify the process of setting up CRUD operations

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),  # Ex: /polls/
-    path('<int:question_id>/', views.detail, name='detail'),  # Ex: /polls/5/
-    path('<int:question_id>/results/', views.results, name='results'),  # Ex: /polls/5/results/
-    path('<int:question_id>/vote/', views.results, name='vote'),  # Ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),  # Ex: /polls/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),  # Ex: /polls/5/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),  # Ex: /polls/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # Ex: /polls/5/vote/
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,19 +4,26 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 from .models import Question, Choice
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, template_name="polls/index.html", context=context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    model = Question
+    context_object_name = Question
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
+
+
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Question
 
 
 def vote(request, question_id):
@@ -36,8 +43,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
